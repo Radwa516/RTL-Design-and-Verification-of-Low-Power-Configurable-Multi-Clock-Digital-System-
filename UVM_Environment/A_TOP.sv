148bd1f9925a23288c368e2298996d6cda3e6846
Radwa516 <104581260+Radwa516@users.noreply.github.com>
2025-05-05 06:21:34 +0300

    Update A_TOP.sv

--- UVM_Environment/A_TOP.sv
+++ UVM_Environment/A_TOP.sv
@@ -11,8 +11,9 @@ bit REF_CLK;
 
 INTF_UART U_intf (UART_CLK, REF_CLK);
 
-System_Top # (.DATA_WID_S(DATA_WIDTH_TB),.Addr_Width_S(ADDR_WIDTH_TB), .NUM_STAGES_S(NUM_STAGES_TB)) DUT 
-(
+System_Top # (.DATA_WID_S(DATA_WIDTH_TB),
+	      .Addr_Width_S(ADDR_WIDTH_TB), 
+	      .NUM_STAGES_S(NUM_STAGES_TB)) DUT (
 .UART_CLK(UART_CLK),
 .REF_CLK(REF_CLK),
 .U_intf(U_intf)
@@ -27,38 +28,38 @@ assign U_intf.RX_Valid  = U0_UART.DATA_VALID;
 
 initial 
 	begin
-		initialization();
+	   initialization();
 
-		uvm_config_db#(virtual INTF_UART)::set(null,"uvm_test_top","my_vif",U_intf);
-		run_test("Test");
+	   uvm_config_db#(virtual INTF_UART)::set(null,"uvm_test_top","my_vif",U_intf);
+	   run_test("Test");
 	end   
 
 ////////////////////initialization of the inputs///////////////////
 task initialization;
      begin
-		U_intf.RST = 'b0;
+	U_intf.RST = 'b0;
         U_intf.RX_IN = 'b1;
      end
 endtask
 
 ////////////////////UART Clock Generation/////////////////////////
-	initial 
+initial 
+	begin
+	  UART_CLK = 0;
+	  forever 
 		begin
-			UART_CLK = 0;
-			forever 
-				begin
-					#(UART_CLK_PER/2) UART_CLK = ~UART_CLK;
-				end
+		  #(UART_CLK_PER/2) UART_CLK = ~UART_CLK;
 		end
+	end
 
 ////////////////////REF Clock Generation//////////////////////////
-	initial 
+initial 
+	begin
+	  REF_CLK = 0;
+	  forever 
 		begin
-			REF_CLK = 0;
-			forever 
-				begin
-					#(REF_CLK_PER/2) REF_CLK = ~REF_CLK;
-				end
+		  #(REF_CLK_PER/2) REF_CLK = ~REF_CLK;
 		end
+	end
 
 endmodule
