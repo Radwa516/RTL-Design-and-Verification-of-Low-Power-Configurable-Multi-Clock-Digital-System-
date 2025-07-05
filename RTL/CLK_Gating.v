e78a52da7b988ca4bace378c6d0b423a4be0f7e2
Radwa516 <104581260+Radwa516@users.noreply.github.com>
2025-07-05 15:17:59 +0300

    Update CLK_Gating.v

--- RTL/CLK_Gating.v
+++ RTL/CLK_Gating.v
@@ -1,10 +1,10 @@
 module CLK_Gating (
-input wire  CLK_REF, 
-input wire  Enable,
-output wire OUT_CLK
+ input wire  CLK_REF, 
+ input wire  Enable,
+ output wire OUT_CLK
 );
 
-reg latch_out;
+ reg latch_out;
 
 always@(CLK_REF or Enable)
  begin
@@ -12,5 +12,5 @@ always@(CLK_REF or Enable)
      latch_out <= Enable;
  end
 
-assign OUT_CLK = CLK_REF && latch_out;
+ assign OUT_CLK = CLK_REF && latch_out;
 endmodule
